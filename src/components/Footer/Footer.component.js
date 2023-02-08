fea50ebe59ac469784bbe166c7cb32edc024a145
Hussein Khaleefah <husseinosamah2@gmail.com>
2023-02-08 23:00:42 +0100

    U

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import "./Footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import {
 //   faEnvelope,
 //   faPhone,
